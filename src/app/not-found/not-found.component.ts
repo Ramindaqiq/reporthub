50bcfe982659c7c8196ba43459686763085b02af
Ramindaqiq <ramindaqiq406@gmail.com>
2018-03-25 22:55:35 +0430

    Update not-found.component.ts

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -2,8 +2,7 @@ import { Component,OnInit } from '@angular/core';
 
 @Component({
     selector: 'not-found-cmp',
-    templateUrl: 'not-found.component.html',
-    styleUrls: ['not-found.component.css']
+    templateUrl: 'not-found.component.html'
 })
 
 export class NotFoundComponent implements OnInit {
