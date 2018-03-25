2433e80746f3fadafd4cad00b4a0aeb0383033ff
Ramindaqiq <ramindaqiq406@gmail.com>
2018-03-25 22:55:07 +0430

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ declare var $:any;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 
 export class AppComponent{}
